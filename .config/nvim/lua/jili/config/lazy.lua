fb3cdc8faf20ae603deb87d31b4a9acdf3639604
Jellli <dt@live.it>
2025-02-10 10:36:45 +0800

    update config

--- .config/nvim/lua/jili/config/lazy.lua
+++ .config/nvim/lua/jili/config/lazy.lua
@@ -1,15 +1,28 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-	local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 	if vim.v.shell_error ~= 0 then
-		error('Error cloning lazy.nvim:\n' .. out)
+		error("Error cloning lazy.nvim:\n" .. out)
 	end
 end
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup({
-	spec = {
-		{import = "jili.plugins"}
-	},
+require("lazy").setup({
+	require("jili.plugins.autocmp"),
+	require("jili.plugins.blankline"),
+	require("jili.plugins.color-scheme"),
+	require("jili.plugins.comment"),
+	-- require("jili.plugins.codeium"),
+	require("jili.plugins.formatter"),
+	require("jili.plugins.gitsigns"),
+	require("jili.plugins.lsp"),
+	require("jili.plugins.lua-line"),
+	require("jili.plugins.mini"),
+	require("jili.plugins.misc"),
+	require("jili.plugins.neo-tree"),
+	require("jili.plugins.telescope"),
+	require("jili.plugins.treesitter"),
+	require("jili.plugins.vim-tmux-navigator"),
+	require("jili.plugins.which-key"),
 	checker = { enabled = true },
 })
--- .zshrc
+++ .zshrc
@@ -76,6 +76,6 @@ zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
 
 # Aliases
 alias ls='ls --color'
-# alias vim='nvim'
+alias vim='nvim'
 alias c='clear'
-
+alias lg='lazygit'
