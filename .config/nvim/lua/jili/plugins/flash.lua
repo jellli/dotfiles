3422fb2e356180c3dd51176a84871ad46fe7e33d
Jellli <dt@live.it>
2025-02-14 09:38:04 +0800

    add jump to line and trouble

--- .config/nvim/lua/jili/plugins/flash.lua
+++ .config/nvim/lua/jili/plugins/flash.lua
@@ -16,5 +16,13 @@ return {
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    {"L", mode = "n", function ()
+      require("flash").jump({
+        search = { mode = "search", max_length = 0 },
+        label = { after = { 0, 0 } },
+        pattern = "^"
+      })
+    end
+    , desc = "Flash" },
   },
 }
