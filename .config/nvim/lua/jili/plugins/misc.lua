05a84a008212ea32a609620414a2355b7c6509dd
Jellli <dt@live.it>
2025-02-26 14:45:50 +0800

    disable headings render and faster cursor animation

--- .config/nvim/lua/jili/plugins/misc.lua
+++ .config/nvim/lua/jili/plugins/misc.lua
@@ -8,6 +8,13 @@ return {
 			preview = {
 				-- icon_provider = "mini",
 				enable = true,
+				-- filetypes = { "md" },
+				-- ignore_previews = {},
+			},
+			markdown = {
+				headings = {
+					enable = false,
+				},
 			},
 		},
 	},
@@ -17,7 +24,11 @@ return {
 	},
 	{
 		"sphamba/smear-cursor.nvim",
-		opts = {},
+		opts = { -- Default  Range
+			stiffness = 0.8, -- 0.6      [0, 1]
+			trailing_stiffness = 0.5, -- 0.3      [0, 1]
+			distance_stop_animating = 0.5, -- 0.1      > 0
+		},
 	},
 	{
 		"m4xshen/hardtime.nvim",
