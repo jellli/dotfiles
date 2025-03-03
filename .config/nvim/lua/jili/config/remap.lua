e1207799de12af78f3032115755fee64aac3ecf0
Jellli <dt@live.it>
2025-03-03 20:48:44 +0800

    update

--- .config/nvim/lua/jili/config/remap.lua
+++ .config/nvim/lua/jili/config/remap.lua
@@ -85,8 +85,12 @@ vim.keymap.set("n", "c", '"_c', opts)
 vim.keymap.set("n", "C", '"_C', opts)
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "[d", function()
+	vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", function()
+	vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
