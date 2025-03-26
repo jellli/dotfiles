25402eaf7c7e55d756ade6cf34fd2a692e71f535
Jellli <dt@live.it>
2025-03-26 17:55:15 +0800

    update

--- .config/nvim/lua/jili/plugins/color-scheme.lua
+++ .config/nvim/lua/jili/plugins/color-scheme.lua
@@ -1,5 +1,50 @@
 return {
 	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup({
+				transparent = true,
+				terminalColors = false,
+				colors = {
+					theme = {
+						all = {
+							ui = {
+								bg_gutter = "none",
+							},
+						},
+					},
+				},
+				overrides = function(colors)
+					local theme = colors.theme
+					return {
+						NormalFloat = { bg = "none" },
+						FloatBorder = { bg = "none" },
+						FloatTitle = { bg = "none" },
+
+						-- Save an hlgroup with dark background and dimmed foreground
+						-- so that you can use it where your still want darker windows.
+						-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+						NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+						-- Popular plugins that open floats will link to NormalFloat by default;
+						-- set their background accordingly if you wish to keep them dark and borderless
+						LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+						MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+					}
+				end,
+			})
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
 		"sainnhe/everforest",
 		lazy = false,
 		priority = 1000,
@@ -19,16 +64,16 @@ return {
 			vim.g.gruvbox_material_inlay_hints_background = "none"
 			vim.g.gruvbox_material_transparent_background = 2
 			vim.g.gruvbox_material_cursor = "orange"
-
 			vim.g.gruvbox_material_better_performance = 1
 			vim.g.gruvbox_material_foreground = "material"
 			vim.g.gruvbox_material_background = "soft"
 			vim.g.gruvbox_material_ui_contrast = "low"
 			vim.g.gruvbox_material_float_style = "dim"
-			vim.g.gruvbox_material_enable_italic = 0
-			vim.g.gruvbox_material_disable_italic_comment = 1
+			vim.g.gruvbox_material_enable_italic = 1
+			vim.g.gruvbox_material_disable_italic_comment = 0
 			vim.g.gruvbox_material_disable_terminal_colors = 1
-			vim.cmd.colorscheme("gruvbox-material")
+			vim.g.gruvbox_material_enable_bold = 1
+			-- vim.cmd.colorscheme("gruvbox-material")
 		end,
 	},
 	{
@@ -43,6 +88,7 @@ return {
 				lualine_bold = true,
 				styles = {
 					dim_inactive = true,
+					-- 筛选器表单字段定义
 					sidebars = "transparent",
 					floats = "transparent",
 					comments = { italic = true },
