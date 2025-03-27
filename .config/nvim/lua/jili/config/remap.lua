e7d986cc3f5cd3248244aefcf89ef6bb6444e118
Jellli <dt@live.it>
2025-03-27 13:47:21 +0800

    v0.11-4

--- .config/nvim/lua/jili/config/remap.lua
+++ .config/nvim/lua/jili/config/remap.lua
@@ -4,10 +4,6 @@ vim.g.maplocalleader = " "
 
 -- For conciseness
 local opts = { noremap = true, silent = true }
--- Set jk or kj to escape
--- vim.keymap.set("i", "jk", "<ESC>", opts)
--- vim.keymap.set("i", "kj", "<ESC>", opts)
--- vim.keymap.set({ "i", "n", "v" }, "<C-^>", "<ESC>", opts)
 
 -- insert movement
 vim.keymap.set("i", "<C-h>", "<Left>", opts)
@@ -22,8 +18,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 -- Disable the spacebar key's default behavior in Normal and Visual modes
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", opts)
--- Disable the CapsLock key's default behavior
-vim.keymap.set({ "n", "v", "i" }, "<C-^>", "<Nop>", opts)
 
 -- save file
 vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
@@ -31,9 +25,6 @@ vim.keymap.set("i", "<C-s>", "<cmd> w <CR><Esc>", opts)
 
 -- save file without auto-formatting
 vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts)
-
--- quit file
--- vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
 
 -- delete single character without copying into register
 vim.keymap.set("n", "x", '"_x', opts)
@@ -56,12 +47,10 @@ vim.keymap.set("n", "<Right>", ":vertical resize +10<CR>", opts)
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
 vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts) -- close buffer
--- vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
 
 -- Window management
 vim.keymap.set("n", "<leader>v", "<C-w>v", opts) -- split window vertically
 vim.keymap.set("n", "<leader>h", "<C-w>s", opts) -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", opts) -- make split windows equal width & height
 vim.keymap.set("n", "<leader>xs", ":close<CR>", opts) -- close current split window
 
 -- Navigate between splits
@@ -71,10 +60,10 @@ vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
 
 -- Tabs
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts) -- open new tab
+--[[ vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts) -- open new tab
 vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts) -- close current tab
 vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts) --  go to next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts) --  go to previous tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts) --  go to previous tab ]]
 
 -- Toggle line wrapping
 vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
@@ -87,17 +76,5 @@ vim.keymap.set("v", ">", ">gv", opts)
 vim.keymap.set("v", "p", '"_dP', opts)
 vim.keymap.set("n", "c", '"_c', opts)
 vim.keymap.set("n", "C", '"_C', opts)
-
--- Diagnostic keymaps
-vim.keymap.set("n", "[d", function()
-	vim.diagnostic.jump({ count = -1, float = true })
-end, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", function()
-	vim.diagnostic.jump({ count = 1, float = true })
-end, { desc = "Go to next diagnostic message" })
--- vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
--- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
-
 vim.keymap.set("n", "<leader>bo", "<cmd>:%bd|e#|bd#<cr>", { desc = "Close all buffers but the current one" })
-
 vim.keymap.set("n", "<leader>lz", "<cmd>Lazy<cr>", { desc = "Lazy" })
