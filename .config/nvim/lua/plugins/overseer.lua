71d3987c5f6afbbf8b23a7f5ec94a6188464c277
Jellli <dt@live.it>
2025-11-14 12:06:15 +0800

    update

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -64,9 +64,8 @@ return {
       },
       { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Action recent task" },
       { "<leader>oi", "<cmd>OverseerInfo<cr>", desc = "Overseer Info" },
-      -- { "<leader>ob", "<cmd>OverseerBuild<cr>", desc = "Task builder" },
       { "<leader>oa", "<cmd>OverseerTaskAction<cr>", desc = "Task action" },
-      { "<leader>oc", "<cmd>OverseerClearCache<cr>", desc = "Clear cache" },
+      { "<leader>oc", "<cmd>OverseerRunCmd<cr>", desc = "Run command" },
     },
   },
 }
