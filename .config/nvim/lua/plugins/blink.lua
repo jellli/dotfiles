432f87144e378b2d5f56cd26446fcd711dc39583
Jellli <dt@live.it>
2025-11-25 10:39:42 +0800

    feat(plugins): add blink.indent, migrate file explorer to mini.files, update lockfile

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,5 +1,36 @@
 return {
   {
+    "saghen/blink.indent",
+    --- @module 'blink.indent'
+    --- @type blink.indent.Config
+    opts = {
+      static = {
+        enabled = true,
+        char = "│",
+        whitespace_char = nil, -- inherits from `vim.opt.listchars:get().space` when `nil` (see `:h listchars`)
+        priority = 1,
+        -- specify multiple highlights here for rainbow-style indent guides
+        -- highlights = { 'BlinkIndentRed', 'BlinkIndentOrange', 'BlinkIndentYellow', 'BlinkIndentGreen', 'BlinkIndentViolet', 'BlinkIndentCyan' },
+        highlights = { "BlinkIndent" },
+      },
+      scope = {
+        enabled = true,
+        char = "┆",
+        priority = 1000,
+        -- set this to a single highlight, such as 'BlinkIndent' to disable rainbow-style indent guides
+        -- highlights = { 'BlinkIndentScope' },
+        -- optionally add: 'BlinkIndentRed', 'BlinkIndentCyan', 'BlinkIndentYellow', 'BlinkIndentGreen'
+        highlights = { "BlinkIndentOrange", "BlinkIndentViolet", "BlinkIndentBlue" },
+        -- enable to show underlines on the line above the current scope
+        underline = {
+          enabled = false,
+          -- optionally add: 'BlinkIndentRedUnderline', 'BlinkIndentCyanUnderline', 'BlinkIndentYellowUnderline', 'BlinkIndentGreenUnderline'
+          highlights = { "BlinkIndentOrangeUnderline", "BlinkIndentVioletUnderline", "BlinkIndentBlueUnderline" },
+        },
+      },
+    },
+  },
+  {
     "saghen/blink.cmp",
     dependencies = {
       "xzbdmw/colorful-menu.nvim",
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -116,6 +116,101 @@ local function setup_pick()
   })
 end
 
+local function settup_files()
+  require("mini.files").setup({
+    mappings = {
+      go_in_plus = "<cr>",
+    },
+  })
+
+  local map_split = function(buf_id, lhs, direction)
+    local rhs = function()
+      -- Make new window and set it as target
+      local cur_target = MiniFiles.get_explorer_state().target_window
+      local new_target = vim.api.nvim_win_call(cur_target, function()
+        vim.cmd(direction .. " split")
+        return vim.api.nvim_get_current_win()
+      end)
+
+      MiniFiles.set_target_window(new_target)
+      MiniFiles.close()
+
+      -- This intentionally doesn't act on file under cursor in favor of
+      -- explicit "go in" action (`l` / `L`). To immediately open file,
+      -- add appropriate `MiniFiles.go_in()` call instead of this comment.
+    end
+
+    -- Adding `desc` will result into `show_help` entries
+    local desc = "Split " .. direction
+    vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+  end
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "MiniFilesBufferCreate",
+    callback = function(args)
+      local buf_id = args.data.buf_id
+      -- Tweak keys to your liking
+      map_split(buf_id, "<C-s>", "belowright horizontal")
+      map_split(buf_id, "<C-v>", "belowright vertical")
+      map_split(buf_id, "<C-t>", "tab")
+    end,
+  })
+
+  -- Yank in register full path of entry under cursor
+  local yank_path = function()
+    local path = (MiniFiles.get_fs_entry() or {}).path
+    if path == nil then
+      return vim.notify("Cursor is not on valid entry")
+    end
+    vim.fn.setreg(vim.v.register, path)
+  end
+
+  -- Open path with system default handler (useful for non-text files)
+  local ui_open = function()
+    vim.ui.open(MiniFiles.get_fs_entry().path)
+  end
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "MiniFilesBufferCreate",
+    callback = function(args)
+      local b = args.data.buf_id
+      vim.keymap.set("n", "gx", ui_open, { buffer = b, desc = "OS open" })
+      vim.keymap.set("n", "gy", yank_path, { buffer = b, desc = "Yank path" })
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("User", {
+    desc = "Notify LSPs that a file was renamed",
+    pattern = { "MiniFilesActionRename", "MiniFilesActionMove" },
+    callback = function(args)
+      local changes = {
+        files = {
+          {
+            oldUri = vim.uri_from_fname(args.data.from),
+            newUri = vim.uri_from_fname(args.data.to),
+          },
+        },
+      }
+      local will_rename_method, did_rename_method = "workspace/willRenameFiles", "workspace/didRenameFiles"
+      local clients = vim.lsp.get_clients()
+      for _, client in ipairs(clients) do
+        if client:supports_method(will_rename_method) then
+          local res = client:request_sync(will_rename_method, changes, 1000, 0)
+          if res and res.result then
+            vim.lsp.util.apply_workspace_edit(res.result, client.offset_encoding)
+          end
+        end
+      end
+
+      for _, client in ipairs(clients) do
+        if client:supports_method(did_rename_method) then
+          client:notify(did_rename_method, changes)
+        end
+      end
+    end,
+  })
+end
+
 return {
   {
     "dmtrKovalenko/fff.nvim",
@@ -132,8 +227,23 @@ return {
       setup_pairs()
       setup_extra()
       setup_pick()
+      settup_files()
     end,
     keys = {
+      {
+        "<leader>e",
+        function()
+          local bufname = vim.api.nvim_buf_get_name(0)
+          local path = vim.fn.fnamemodify(bufname, ":p")
+
+          -- Noop if the buffer isn't valid.
+          if path and vim.uv.fs_stat(path) then
+            require("mini.files").open(bufname, false)
+          end
+        end,
+        desc = "File explorer",
+      },
+
       {
         "<leader><leader>",
         mini_bonus.fff.run,
