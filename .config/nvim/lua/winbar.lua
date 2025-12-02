b029a6e4aefb951428d64e43811b1b33cbd91e3b
Jellli <dt@live.it>
2025-12-02 11:06:59 +0800

    dont show winbar in some filetype

--- .config/nvim/lua/winbar.lua
+++ .config/nvim/lua/winbar.lua
@@ -22,9 +22,16 @@ Winbar.render = function(bufnr)
   })
 end
 
+local excludes =
+  { "fugitive", "lazy", "minipick", "minifiles", "codecompanion", "OverseerList", "OverseerForm", "snacks_input" }
+
 vim.api.nvim_create_autocmd({ "BufEnter", "BufModifiedSet" }, {
   group = vim.api.nvim_create_augroup("j/winbar", { clear = true }),
   callback = function(event)
+    if vim.tbl_contains(excludes, vim.bo.filetype) then
+      return
+    end
+
     vim.wo.winbar = Winbar.render(event.buf)
   end,
 })
