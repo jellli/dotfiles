d21efd61269b4762488bea8dd38bfca03bd568b3
Jellli <dt@live.it>
2025-12-03 11:08:37 +0800

    remove im-select.nvim and add automatic input method switching

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -75,3 +75,64 @@ vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
     vim.system({ "tmux", "rename-window", vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t") })
   end,
 })
+
+local function get_os()
+  if vim.fn.has("macunix") == 1 then
+    return "macOS"
+  elseif vim.fn.has("win32") == 1 then
+    return "Windows"
+  elseif vim.fn.has("wsl") == 1 then
+    return "WSL"
+  else
+    return "Linux"
+  end
+end
+
+local function get_switch_cmd()
+  local os = get_os()
+  local cmd = {
+    lhs = {},
+    rhs = {},
+  }
+
+  if os == "macOS" then
+    table.insert(cmd.lhs, "macism")
+    table.insert(cmd.rhs, "com.apple.keylayout.ABC")
+  elseif os == "Windows" or os == "WSL" then
+    table.insert(cmd.lhs, "im-select.exe")
+    table.insert(cmd.rhs, "1033")
+  else
+    if vim.fn.executable("fcitx5-remote") then
+      table.insert(cmd.lhs, "fcitx5-remote")
+      table.insert(cmd.rhs, "-s")
+      table.insert(cmd.rhs, "keyboard-us")
+    else
+      return
+    end
+  end
+  return cmd
+end
+vim.api.nvim_create_autocmd({ "InsertLeave", "CmdlineLeave" }, {
+  group = vim.api.nvim_create_augroup("auto_switch_input_method", { clear = true }),
+  callback = function()
+    local cmd = get_switch_cmd()
+    if not cmd then
+      vim.notify("No supported input method executable found for your OS.", vim.log.levels.WARN)
+      return
+    end
+
+    local handle
+    ---@diagnostic disable-next-line: missing-fields
+    handle = uv.spawn(cmd.lhs[1], { args = cmd.rhs }, function(code)
+      if handle and not handle:is_closing() then
+        handle:close()
+      end
+      if code ~= 0 then
+        vim.notify("Input method switch failed (process exited with non-zero code).", vim.log.levels.WARN)
+      end
+    end)
+    if not handle then
+      vim.notify("Failed to start input method switch process.", vim.log.levels.WARN)
+    end
+  end,
+})
