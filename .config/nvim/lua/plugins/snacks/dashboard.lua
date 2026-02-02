17dfebde92100659f7345cff6d1899e59174d36c
Jellli <dt@live.it>
2026-02-02 11:20:47 +0800

    update dashboard

--- .config/nvim/lua/plugins/snacks/dashboard.lua
+++ .config/nvim/lua/plugins/snacks/dashboard.lua
@@ -1,16 +1,3 @@
-local function get_version()
-  local version = vim.version()
-  local version_str = string.format(
-    "v%d.%d.%d (%s)",
-    version.major,
-    version.minor,
-    version.patch,
-    version.prerelease and "Nightly" or "Stable"
-  )
-
-  return version_str
-end
---- @type snacks.dashboard.Opts
 return {
   enabled = true,
   preset = {
@@ -30,10 +17,14 @@ return {
     ]],
   },
   sections = {
-    { section = "header", padding = 1, height = 2 },
-    { title = "Recent Files", section = "recent_files", cwd = true, indent = 2, padding = 1 },
-    { text = { { get_version(), hl = "Special" } }, padding = 1, height = 2, align = "center" },
-    { section = "startup" },
+    { section = "header" },
+    {
+      section = "recent_files",
+      cwd = true,
+      limit = 2,
+    },
   },
-  formats = {},
+  formats = {
+    header = { "%s", align = "center", hl = "Comment" },
+  },
 }
