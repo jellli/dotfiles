fb48445b54dd7b16d74d87bac8671bcab7cb4f62
Jellli <dt@live.it>
2026-02-02 17:37:43 +0800

    jump without offset_encoding

--- .config/nvim/lua/picker/lsp.lua
+++ .config/nvim/lua/picker/lsp.lua
@@ -11,19 +11,41 @@ local M = {}
 
 ---@param scope LspRequestType
 function M.lsp_picker(scope)
-  M.get_locations(scope, function(items)
-    if not items or #items == 0 then
+  M.get_locations(scope, function(all_items)
+    if not all_items or #all_items == 0 then
       vim.notify("No locations found", vim.log.levels.WARN)
       return
     end
 
-    if #items == 1 then
-      local item = items[1]
-      vim.lsp.util.show_document(item.user_data, item.offset_encoding, {
-        reuse_win = true,
-        focus = true,
-      })
-      vim.cmd("normal! zz")
+    if #all_items == 1 then
+      local win = vim.api.nvim_get_current_win()
+      local from = vim.fn.getpos(".")
+      local tagname = vim.fn.expand("<cword>")
+      local item = all_items[1]
+      local b = item.bufnr or vim.fn.bufadd(item.filename)
+
+      -- Save position in jumplist
+      vim.cmd("normal! m'")
+      -- Push a new item into tagstack
+      local tagstack = { { tagname = tagname, from = from } }
+      vim.fn.settagstack(vim.fn.win_getid(win), { items = tagstack }, "t")
+
+      vim.bo[b].buflisted = true
+      local w = win
+      if vim.api.nvim_win_get_buf(w) ~= b then
+        w = vim.fn.bufwinid(b)
+        w = w >= 0 and w or vim.fn.win_findbuf(b)[1] or win
+        if w ~= win then
+          vim.api.nvim_set_current_win(w)
+        end
+      end
+      vim.api.nvim_win_set_buf(w, b)
+      vim.api.nvim_win_set_cursor(w, { item.lnum, item.col - 1 })
+      vim._with({ win = w }, function()
+        -- Open folds under the cursor
+        vim.cmd("normal! zv")
+        vim.cmd("normal! zz")
+      end)
       return
     end
 
@@ -31,8 +53,12 @@ function M.lsp_picker(scope)
       name = "ok",
       source = {
         name = scope,
-        items = items,
+        items = all_items,
         show = M.show,
+        choose = function(item)
+          item.path = item.filename
+          MiniPick.default_choose(item)
+        end,
       },
       window = {
         config = {
@@ -70,9 +96,6 @@ function M.get_locations(scope, cb)
       else
         local client = assert(vim.lsp.get_client_by_id(cilent_id))
         local items = vim.lsp.util.locations_to_items(result or {}, client.offset_encoding)
-        for i, _ in ipairs(items) do
-          items[i].offset_encoding = client.offset_encoding
-        end
         vim.list_extend(all_items, items)
       end
     end
