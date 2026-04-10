1665b864beffa94af829568d261e24cdcc281ece
Jellli <dt@live.it>
2026-04-10 11:11:55 +0800

    replace flash.nvim with leap.nvim and add tiny-cmdline

--- .config/nvim/plugin/flash.lua
+++
@@ -1,7 +0,0 @@
-vim.pack.add({
-	"https://github.com/folke/flash.nvim",
-})
-
-require("flash").setup({})
-
-Jili.keymap({ "n", "x", "o" }, "s", require("flash").jump, "Flash Jump")
---
+++ .config/nvim/plugin/leap.lua
@@ -0,0 +1,14 @@
+vim.pack.add({
+	"https://codeberg.org/andyg/leap.nvim",
+})
+
+vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+vim.keymap.set("n", "S", "<Plug>(leap-anywhere)")
+vim.keymap.set({ "x", "o" }, "R", function()
+	require("leap.treesitter").select({
+		opts = require("leap.user").with_traversal_keys("R", "r"),
+	})
+end)
+
+require("leap.user").set_backdrop_highlight("Comment")
+require("leap").opts.preview = false
