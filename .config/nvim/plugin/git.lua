94b468c9710913cf582c4c0e0378b30dd4dc05fd
Jellli <dt@live.it>
2026-04-16 16:42:09 +0800

    update git keymap

--- .config/nvim/plugin/git.lua
+++ .config/nvim/plugin/git.lua
@@ -6,48 +6,31 @@ local function load()
 		"https://github.com/esmuellert/codediff.nvim",
 	})
 
-	local gs = require("gitsigns")
-	gs.setup({
+	require("gitsigns").setup({
 		current_line_blame = false,
 		gh = true,
-		on_attach = function()
-			local keymap = Jili.keymap
-			keymap("n", "[h", function()
-				gs.nav_hunk("prev", {
-					navigation_message = false,
-				})
-			end, "Previous hunk")
-			keymap("n", "]h", function()
-				gs.nav_hunk("next", {
-					navigation_message = false,
-				})
-			end, "Next hunk")
-
-			keymap("n", "<leader>gb", gs.blame, "Blame line")
-			keymap("n", "<leader>tlb", gs.toggle_current_line_blame, "Toggle current line blame")
-			keymap("n", "<leader>twb", gs.toggle_word_diff, "Toggle word diff")
-			keymap("n", "<leader>hp", gs.preview_hunk_inline, "Preview hunk")
-			keymap("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
-			keymap("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
-
-			keymap("n", "<leader>ge", "<cmd>Gedit<cr>", "Git edit")
-			keymap("n", "<leader>gs", "<cmd>vert Git<cr>", "Git status")
-			keymap("n", "<leader>ga", "<cmd>Git add %<cr>", "Git add current file")
-			keymap("n", "<leader>gw", "<cmd>Gwrite<cr>", "Git write")
-			keymap("n", "<leader>gr", "<cmd>Gread<cr>", "Git read")
-			keymap("n", "<leader>gc", "<cmd>Git commit -v -q<cr>", "Git commit")
-			keymap("n", "<leader>gt", "<cmd>Git commit -v -q %:p<cr>", "Git commit current file")
-			keymap("n", "<leader>gd", "<cmd>Gvdiffsplit<cr>", "Git diff")
-			keymap(
-				"n",
-				"<leader>gl",
-				"<cmd>vert Git --paginate log --graph --pretty=format:'%C(magenta)%h %C(white) %an (%ar)%C(auto) %D%n%s%n'<cr>",
-				"Git log"
-			)
-			keymap("n", "<leader>gP", "<cmd>Git push<cr>", "Git push")
-			keymap("n", "<leader>gp", "<cmd>Git pull<cr>", "Git pull")
-		end,
 	})
+
+	local keymap = Jili.keymap
+	keymap("n", "[c", "<cmd>Gitsigns nav_hunk prev --target=all --navigation_message=false<cr>", "Previous hunk")
+	keymap("n", "]c", "<cmd>Gitsigns nav_hunk next --target=all --navigation_message=false<cr>", "Next hunk")
+
+	keymap("n", "<leader>gb", "<cmd>Gitsigns blame<cr>", "Blame line")
+	keymap("n", "<leader>hp", "<cmd>Gitsigns preview_hunk_inline<cr>", "Preview hunk")
+	keymap("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<cr>", "Stage hunk")
+	keymap("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<cr>", "Reset hunk")
+
+	keymap("n", "<leader>gg", "<cmd>Git<cr>", "Git status")
+
+	keymap("n", "<leader>gw", "<cmd>Gwrite<cr>", "Git write current buffer(stage)")
+	keymap("n", "<leader>gr", "<cmd>Gread<cr>", "Git read (reset)")
+
+	keymap("n", "<leader>gc", "<cmd>Git commit -v -q<cr>", "Git commit")
+	keymap("n", "<leader>gh", "<cmd>CodeDiff history<cr>", "Git history")
+	keymap("n", "<leader>gl", "<cmd>vert Git log --oneline --graph<cr>", "Git log")
+
+	keymap("n", "<leader>gP", "<cmd>Git push<cr>", "Git push")
+	keymap("n", "<leader>gp", "<cmd>Git pull<cr>", "Git pull")
 end
 
 vim.schedule(load)
